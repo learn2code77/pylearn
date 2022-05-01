f7b06300187e99ca64301c4d06dbdcf866d10bba
learn2code77 <learn2code77@gmail.com>
2022-05-01 11:49:52 +0530

    New directory for Matthes

---
+++ matthes/data_types/f_strings.py
@@ -0,0 +1,9 @@
+First_Name = "Siddesh"
+Last_Name = "Prabhu"
+Full_Name = f"{First_Name} {Last_Name}"
+
+#one of the way to output
+print("Hi, how are you ", Full_Name)
+
+#other way to output
+print(f"Hi, how are you {Full_Name}")
