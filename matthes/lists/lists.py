ab1d4d23af7b06cb2517eaa3bc45dbef2805102b
learn2code77 <learn2code77@gmail.com>
2022-05-08 10:58:05 +0530

    new_code

---
+++ matthes/lists/lists.py
@@ -0,0 +1,12 @@
+friends = ["raghav", "balu", "rahul", "Rama", "Chander"]
+
+print(friends)
+
+n = 0
+for friend in friends:
+    print(f'Hi, How are you {friends[n]}')
+    n = n + 1
+
+
+########################################################################
+
