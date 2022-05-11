2816d097d52eda81ce8e6c4d59eb36f7015de447
learn2code77 <learn2code77@gmail.com>
2022-05-11 10:57:42 +0530

    new_code

---
+++ practise/list/fibonacci.py
@@ -0,0 +1,21 @@
+'''https://www.practicepython.org/exercise/2014/04/30/13-fibonacci.html
+Write a program that asks the user how many Fibonnaci numbers to generate and then generates them.
+Take this opportunity to think about how you can use functions. Make sure to ask the user to enter
+the number of numbers in the sequence to generate.(Hint: The Fibonnaci seqence is a sequence of numbers
+where the next number in the sequence is the sum of the previous two numbers in the sequence.
+The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …)'''
+
+def fibonacci(value):
+    series = []
+    for i in range(0, value+1):
+        series.append(i)
+    return series
+
+    j = 0
+    fab_series = []
+    for item in series:
+        fab_series.append(j + series[]
+
+
+
+print(fibonacci(5))
